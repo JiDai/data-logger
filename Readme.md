f570d8ed845b60e570765ab50f68cfef50f7d49c
Jordi Dosne <f_github@jidai.fr>
2024-02-13 22:04:58 +0100

    feat: first version seems ok

--- Readme.md
+++ Readme.md
@@ -1,25 +1,32 @@
-# An addon to inspect all XHR/Fetch and graphql call  
+# An addon to inspect all XHR/Fetch and graphql call
 
 ## Architecture
 
 In `ext-config` folder you will find all the addon "core" files.
 
-The `src` folder contains the sources files for the application of the devtools panel representing the main UI of the module. 
-The application is built on top of tailwind and daisyUI. Built files will take place on the `/ext-config/devtools-panel/app` folder. 
+The `src` folder contains the sources files for the application of the devtools panel representing the main UI of the module.
+The application is built on top of tailwind and daisyUI. Built files will take place on the `/ext-config/devtools-panel/app` folder.
 
 
 ## Install
 
 `pnpm install`
 
-## Build 
+## Build
 
 `pnpm run build`
 
 The build task will generate files in the `/ext-config/devtools-panel/app` folder to be used by the extensions as a devtools panel.
 
-## Develop 
+## Develop
 
 `pnpm run dev`
 
 The task will build with watching.
+
+## Todo
+
+- Use `formatData` to handle more response type
+- Global search
+- JSONPath, XPath selector or equivalent
+- Filter on request list
--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,325 +1,298 @@
-import type {DefinitionNode, FieldNode, InlineFragmentNode, OperationDefinitionNode, OperationTypeNode, SelectionNode} from 'graphql'
-import type {Header, Param} from 'har-format'
-import {Kind, parse} from 'graphql'
-import {getQuery, parseURL} from 'ufo'
-import {randomUUID} from 'uncrypto'
-import httpStatus from 'http-status'
-import prettier from 'prettier/standalone'
+import type { DefinitionNode, FieldNode, InlineFragmentNode, OperationDefinitionNode, OperationTypeNode, SelectionNode } from "graphql";
+import type { Header, Param } from "har-format";
+import { Kind, parse } from "graphql";
+import { getQuery, parseURL } from "ufo";
+import { randomUUID } from "uncrypto";
+import httpStatus from "http-status";
+import prettier from "prettier/standalone";
 // @ts-expect-error - no types
-import htmlParser from 'prettier/parser-html'
-import xmlParser from '@prettier/plugin-xml'
-import type {BaseEntry, Entry, GQLEntry, HAREntry, HTTPEntry} from '../types'
+import htmlParser from "prettier/parser-html";
+import xmlParser from "@prettier/plugin-xml";
+import type { BaseEntry, Entry, GQLEntry, HAREntry, HTTPEntry } from "../types";
 
 class ParseResponseError extends Error {
-    constructor(message = 'Cant\'t parse response content') {
-        super(message)
-        this.name = 'ParseResponseError'
-    }
+	constructor(message = "Cant't parse response content") {
+		super(message);
+		this.name = "ParseResponseError";
+	}
 }
 
 interface ParsedQueryDefinition {
-    name: string | undefined
-    kind: Kind.OPERATION_DEFINITION
-    type: OperationTypeNode
-    operations: string[]
+	name: string | undefined;
+	kind: Kind.OPERATION_DEFINITION;
+	type: OperationTypeNode;
+	operations: string[];
 }
 
 interface ParsedQuery {
-    query: string
-    data: ParsedQueryDefinition[]
-    variables?: any
-    operationName?: string
-    batch?: {
-        length: number
-        count: number
-    }
+	query: string;
+	data: ParsedQueryDefinition[];
+	variables?: any;
+	operationName?: string;
+	batch?: {
+		length: number;
+		count: number;
+	};
 }
 
 function isInlineFragment(node: SelectionNode): node is InlineFragmentNode {
-    return node.kind === Kind.INLINE_FRAGMENT
+	return node.kind === Kind.INLINE_FRAGMENT;
 }
 
 function isField(node: SelectionNode): node is FieldNode {
-    return node.kind === Kind.FIELD
+	return node.kind === Kind.FIELD;
 }
 
 function isContentType(request: { headers: Header[] }, contentType: string) {
-    return request.headers.some(
-        ({name, value}) =>
-            name.toLowerCase() === 'content-type'
-            && value.split(';').at(0)?.toLowerCase() === contentType.toLowerCase(),
-    )
+	return request.headers.some(({ name, value }) => name.toLowerCase() === "content-type" && value.split(";").at(0)?.toLowerCase() === contentType.toLowerCase());
 }
 
 function isOperationDefinition(node: DefinitionNode): node is OperationDefinitionNode {
-    return node.kind === Kind.OPERATION_DEFINITION
+	return node.kind === Kind.OPERATION_DEFINITION;
 }
 
 function isValidQuery(query: string) {
-    try {
-        parse(query, {noLocation: true})
+	try {
+		parse(query, { noLocation: true });
 
-        return true
-    } catch (_) {
-        return false
-    }
+		return true;
+	} catch (_) {
+		return false;
+	}
 }
 
 function getName(node: SelectionNode): string {
-    if (isInlineFragment(node) && node.typeCondition)
-        return `InlineFragment if ${node.typeCondition.name.value}`
-
-    else if (isField(node))
-        return node.alias ? `${node.alias.value}: ${node.name.value}` : node.name.value
-
-    else
-        return 'Anonymous'
+	if (isInlineFragment(node) && node.typeCondition) return `InlineFragment if ${node.typeCondition.name.value}`;
+	else if (isField(node)) return node.alias ? `${node.alias.value}: ${node.name.value}` : node.name.value;
+	else return "Anonymous";
 }
 
 function parseOperation(definition: SelectionNode) {
-    return {
-        kind: definition.kind,
-        name: getName(definition),
-    }
+	return {
+		kind: definition.kind,
+		name: getName(definition),
+	};
 }
 
 function getQueryValue(name: string, params: Param[] = []) {
-    const {value} = params.find(param => param.name === name) || {}
+	const { value } = params.find((param) => param.name === name) || {};
 
-    return value && decodeURIComponent(value)
+	return value && decodeURIComponent(value);
 }
 
 function parseQuery(query: string): ParsedQueryDefinition[] {
-    const {definitions} = parse(query, {noLocation: true})
+	const { definitions } = parse(query, { noLocation: true });
 
-    return definitions.filter(isOperationDefinition).map(definition => ({
-        name: definition.name?.value,
-        kind: definition.kind,
-        type: definition.operation,
-        operations: definition.selectionSet.selections
-            .map(operation => parseOperation(operation))
-            .map(operation => operation.name),
-    }))
+	return definitions.filter(isOperationDefinition).map((definition) => ({
+		name: definition.name?.value,
+		kind: definition.kind,
+		type: definition.operation,
+		operations: definition.selectionSet.selections.map((operation) => parseOperation(operation)).map((operation) => operation.name),
+	}));
 }
 
 export function isGQLEntry(entry: Entry): entry is GQLEntry {
-    return entry?.type === 'GQL'
+	return entry?.type === "GQL";
 }
 
 export function isHTTP(entry: HAREntry) {
-    return ['xhr', 'fetch', 'preflight'].includes(entry._resourceType)
+	return ["xhr", "fetch", "preflight"].includes(entry._resourceType);
 }
 
 export function isGraphQL(entry: HAREntry) {
-    const {text, params} = entry.request.postData || {}
+	const { text, params } = entry.request.postData || {};
 
-    let query: string | undefined
+	let query: string | undefined;
 
-    if (isContentType(entry.request, 'application/json')) {
-        if (entry.request.method === 'GET') {
-            query = getQueryValue('query', entry.request.queryString)
-        } else if (entry.request.method === 'POST' && text) {
-            try {
-                const json = JSON.parse(text)
-                query = Array.isArray(json) ? json.at(0).query : json.query
-            } catch (e) {
-                return false
-            }
-        }
-    } else if (isContentType(entry.request, 'application/x-www-form-urlencoded') && params) {
-        query = getQueryValue('query', params)
-    }
+	if (isContentType(entry.request, "application/json")) {
+		if (entry.request.method === "GET") {
+			query = getQueryValue("query", entry.request.queryString);
+		} else if (entry.request.method === "POST" && text) {
+			try {
+				const json = JSON.parse(text);
+				query = Array.isArray(json) ? json.at(0).query : json.query;
+			} catch (e) {
+				return false;
+			}
+		}
+	} else if (isContentType(entry.request, "application/x-www-form-urlencoded") && params) {
+		query = getQueryValue("query", params);
+	}
 
-    return !!query && isValidQuery(query)
+	return !!query && isValidQuery(query);
 }
 
 export function parseHTTPEntry(entry: HAREntry): HTTPEntry {
-    const {url, method, postData} = entry.request
-    const {host, pathname, search: queryString} = parseURL(url)
-    const {request, response, ...base} = getEntryInfo(entry)
+	const { url, method, postData } = entry.request;
+	const { host, pathname, search: queryString } = parseURL(url);
+	const { request, response, ...base } = getEntryInfo(entry);
 
-    let body
-    let params
+	let body;
+	let params;
 
-    if (postData) {
-        if (postData.params)
-            params = postData.params
+	if (postData) {
+		if (postData.params && postData.params.length > 0) params = postData.params;
+		else if (postData.text) body = JSON.parse(postData.text);
+	}
+	const getResponse = async () => getContent(entry);
 
-        else if (postData.text)
-            body = JSON.parse(postData.text)
-    }
-    const getResponse = async () => getContent(entry)
-
-    return {
-        type: method,
-        ...base,
-        request: {
-            ...request,
-            name: pathname,
-            host,
-            pathname,
-            queryString,
-            query: getQuery(queryString),
-            body,
-            params,
-        },
-        response: {
-            ...response,
-            getResponse,
-        },
-    }
+	return {
+		type: method,
+		...base,
+		request: {
+			...request,
+			name: pathname,
+			host,
+			pathname,
+			queryString,
+			query: getQuery(queryString),
+			body,
+			params,
+		},
+		response: {
+			...response,
+			getResponse,
+		},
+	};
 }
 
 function isArray(arr: any): arr is any[] {
-    return Array.isArray(arr)
+	return Array.isArray(arr);
 }
 
 export async function parseGQLEntry(entry: HAREntry): Promise<GQLEntry | GQLEntry[]> {
-    const parsedQueries: ParsedQuery[] = []
-    const {postData, queryString} = entry.request
+	const parsedQueries: ParsedQuery[] = [];
+	const { postData, queryString } = entry.request;
 
-    let json: ParsedQuery | null = null
+	let json: ParsedQuery | null = null;
 
-    if (
-        (isContentType(entry.request, 'application/json') && entry.request.method === 'GET')
-        || isContentType(entry.request, 'application/x-www-form-urlencoded')
-    ) {
-        const params = entry.request.method === 'GET' ? queryString : postData?.params
-        const query = getQueryValue('query', params)
-        const variables = getQueryValue('variables', params)
-        const operationName = getQueryValue('operationName', params)
+	if ((isContentType(entry.request, "application/json") && entry.request.method === "GET") || isContentType(entry.request, "application/x-www-form-urlencoded")) {
+		const params = entry.request.method === "GET" ? queryString : postData?.params;
+		const query = getQueryValue("query", params);
+		const variables = getQueryValue("variables", params);
+		const operationName = getQueryValue("operationName", params);
 
-        if (query) {
-            json = {
-                query,
-                data: parseQuery(query),
-                variables,
-                operationName,
-            }
-        }
-    } else if (postData?.text) {
-        try {
-            json = JSON.parse(postData.text)
-        } catch (e: any) {
-            console.warn(
-                `Internal Error Parsing: ${entry}. Message: ${e.message}. Stack: ${e.stack}`,
-            )
-            return []
-        }
-    }
+		if (query) {
+			json = {
+				query,
+				data: parseQuery(query),
+				variables,
+				operationName,
+			};
+		}
+	} else if (postData?.text) {
+		try {
+			json = JSON.parse(postData.text);
+		} catch (e: any) {
+			console.warn(`Internal Error Parsing: ${entry}. Message: ${e.message}. Stack: ${e.stack}`);
+			return [];
+		}
+	}
 
-    [json].flat().forEach((batchItem: any, i: number) => {
-        const {query, operationName} = batchItem
-        let {variables} = batchItem
+	[json].flat().forEach((batchItem: any, i: number) => {
+		const { query, operationName } = batchItem;
+		let { variables } = batchItem;
 
-        try {
-            variables
-                = typeof variables === 'string' ? JSON.parse(variables) : variables
-        } catch (e: any) {
-            console.warn(
-                `Internal Error Parsing: ${entry}. Message: ${e.message}. Stack: ${e.stack}`,
-            )
-            return []
-        }
+		try {
+			variables = typeof variables === "string" ? JSON.parse(variables) : variables;
+		} catch (e: any) {
+			console.warn(`Internal Error Parsing: ${entry}. Message: ${e.message}. Stack: ${e.stack}`);
+			return [];
+		}
 
-        parsedQueries.push({
-            query,
-            data: parseQuery(query),
-            variables,
-            operationName,
-            batch: isArray(json)
-                ? {
-                    length: json.length,
-                    count: i + 1,
-                }
-                : undefined,
-        })
-    })
+		parsedQueries.push({
+			query,
+			data: parseQuery(query),
+			variables,
+			operationName,
+			batch: isArray(json)
+				? {
+						length: json.length,
+						count: i + 1,
+					}
+				: undefined,
+		});
+	});
 
-    const fullParsedQueries = await Promise.all(parsedQueries.map(async (parsedQuery, i) => {
-        const {data, query, variables, operationName, batch} = parsedQuery
-        const {name, type, operations} = data[0]
-        const {request, response, ...base} = getEntryInfo(entry)
+	const fullParsedQueries = await Promise.all(
+		parsedQueries.map(async (parsedQuery, i) => {
+			const { data, query, variables, operationName, batch } = parsedQuery;
+			const { name, type, operations } = data[0];
+			const { request, response, ...base } = getEntryInfo(entry);
 
-        const getResponse = async () => {
-            const body = await getContent(entry)
+			const getResponse = async () => {
+				const body = await getContent(entry);
 
-            return Array.isArray(body) ? body[i] : body
-        }
+				return Array.isArray(body) ? body[i] : body;
+			};
 
-        return {
-            ...base,
-            type: 'GQL',
-            request: {
-                ...request,
-                name: operationName || name,
-                operations,
-                operationType: type,
-                query,
-                variables,
-                batch,
-            },
-            response: {
-                ...response,
-                isError: response.isError || !!(await getResponse()).errors,
-                getResponse,
-            },
-        }
-    }))
+			return {
+				...base,
+				type: "GQL",
+				request: {
+					...request,
+					name: operationName || name,
+					operations,
+					operationType: type,
+					query,
+					variables,
+					batch,
+				},
+				response: {
+					...response,
+					isError: response.isError || !!(await getResponse()).errors,
+					getResponse,
+				},
+			};
+		}),
+	);
 
-    return isArray(json) ? fullParsedQueries : fullParsedQueries[0]
+	return isArray(json) ? fullParsedQueries : fullParsedQueries[0];
 }
 
 async function getContent(entry: HAREntry) {
-    const [body] = await entry.getContent()
+	const [body] = await entry.getContent();
 
-    if (!isContentType(entry.response, 'application/json'))
-        return body
+	if (!isContentType(entry.response, "application/json")) return body;
 
-    try {
-        return JSON.parse(body)
-    } catch (e: any) {
-        console.warn(
-            `Internal Error Parsing: ${entry}. Message: ${e.message}. Stack: ${e.stack}`,
-        )
-        throw new ParseResponseError()
-    }
+	try {
+		return JSON.parse(body);
+	} catch (e: any) {
+		console.warn(`Internal Error Parsing: ${entry}. Message: ${e.message}. Stack: ${e.stack}`);
+		throw new ParseResponseError();
+	}
 }
 
 function getEntryInfo(entry: HAREntry): BaseEntry {
-    const {url, postData, headers: requestHeaders, method} = entry.request
-    const {content, headers: responseHeaders, status} = entry.response
-    const isError = status >= 400 || status === 0
-    const statusMessage = httpStatus[+status]
-    const timestamp = new Date(entry.startedDateTime).getTime()
+	const { url, postData, headers: requestHeaders, method } = entry.request;
+	const { content, headers: responseHeaders, status } = entry.response;
+	const isError = status >= 400 || status === 0;
+	const statusMessage = httpStatus[+status];
+	const timestamp = new Date(entry.startedDateTime).getTime();
 
-    return {
-        id: randomUUID(),
-        time: entry.time,
-        timestamp,
-        request: {
-            url,
-            headers: requestHeaders,
-            mimeType: postData?.mimeType,
-            method,
-        },
-        response: {
-            status,
-            statusMessage,
-            isError,
-            headers: responseHeaders,
-            mimeType: content.mimeType,
-        },
-    }
+	return {
+		id: randomUUID(),
+		time: entry.time,
+		timestamp,
+		request: {
+			url,
+			headers: requestHeaders,
+			mimeType: postData?.mimeType,
+			method,
+		},
+		response: {
+			status,
+			statusMessage,
+			isError,
+			headers: responseHeaders,
+			mimeType: content.mimeType,
+		},
+	};
 }
 
 export function formatData(data: string, mimeType: string) {
-    if (['text/html', 'application/html'].includes(mimeType))
-        return prettier.format(data, {semi: false, parser: 'html', plugins: [htmlParser]})
-    else if (['application/xml', 'text/xml'].includes(mimeType))
-        return prettier.format(data, {semi: false, parser: 'xml', plugins: [xmlParser]})
-    else if (['application/json', 'text/json'].includes(mimeType))
-        return JSON.stringify(data, null, 2)
-    else
-        return data
+	if (["text/html", "application/html"].includes(mimeType)) return prettier.format(data, { semi: false, parser: "html", plugins: [htmlParser] });
+	else if (["application/xml", "text/xml"].includes(mimeType)) return prettier.format(data, { semi: false, parser: "xml", plugins: [xmlParser] });
+	else if (["application/json", "text/json"].includes(mimeType)) return JSON.stringify(data, null, 2);
+	else return data;
 }
