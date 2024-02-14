a5d195e3ede83350b672dcb069680e0bccea6c4e
Jordi Dosne <f_github@jidai.fr>
2024-02-14 12:46:36 +0100

    feat(JID-134): harmonize app name

--- ext-config/devtools-panel/script.js
+++ ext-config/devtools-panel/script.js
@@ -1,18 +1,18 @@
 function handleShown() {
-  console.log("panel is being shown");
+	console.log("panel is being shown");
 }
 
 function handleHidden() {
-  console.log("panel is being hidden");
+	console.log("panel is being hidden");
 }
 
 browser.devtools.panels
-  .create(
-    "Data inspector", // title
-    "../../icons/icon.png", // icon
-    "./app/index.html", // content
-  )
-  .then((newPanel) => {
-    newPanel.onShown.addListener(handleShown);
-    newPanel.onHidden.addListener(handleHidden);
-  });
+	.create(
+		"Data logger", // title
+		"../../icons/icon.png", // icon
+		"./app/index.html", // content
+	)
+	.then((newPanel) => {
+		newPanel.onShown.addListener(handleShown);
+		newPanel.onHidden.addListener(handleHidden);
+	});
