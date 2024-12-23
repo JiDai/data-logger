4f2db6f89e984847943e391f62d232127e1ef9f8
Jordi Dosne <f_github@jidai.fr>
2024-12-23 14:05:39 +0100

    improve: view selected filters

--- src/Settings.svelte
+++ src/Settings.svelte
@@ -1,5 +1,8 @@
 <script lang="ts">
-	import { empty, setSettings } from './store';
+	import { empty, setSettings, settings } from './store';
+	import { faCheckCircle } from '@fortawesome/free-regular-svg-icons';
+	import Fa from 'svelte-fa';
+	import { get } from 'svelte/store';
 
 	function clearList() {
 		empty();
@@ -12,7 +15,8 @@
 	<ul class="menu dropdown-content bg-base-100 rounded-box z-[1] w-52 p-2 shadow">
 		<li>
 			<button
-				class="form-control"
+				class="form-control justify-between"
+
 				onclick={() =>
 					setSettings({
 						filters: { GQL: false, JSON: false, XML: false, Img: false, Other: false },
@@ -23,57 +27,62 @@
 		</li>
 		<li>
 			<button
-				class="form-control"
+				class="form-control justify-between"
 				onclick={() =>
 					setSettings({
 						filters: { GQL: true, JSON: false, XML: false, Img: false, Other: false },
 					})}
 			>
 				<span class="label-text">GQL</span>
+				{#if $settings.filters.GQL}<Fa icon={faCheckCircle} class="text-amber-400" />{/if}
 			</button>
 		</li>
 		<li>
 			<button
-				class="form-control"
+				class="form-control justify-between"
 				onclick={() =>
 					setSettings({
 						filters: { GQL: false, JSON: true, XML: false, Img: false, Other: false },
 					})}
 			>
 				<span class="label-text">JSON</span>
+				{#if $settings.filters.JSON}<Fa icon={faCheckCircle} class="text-amber-400" />{/if}
 			</button>
 		</li>
 		<li>
 			<button
-				class="form-control"
+				class="form-control justify-between"
 				onclick={() =>
 					setSettings({
 						filters: { GQL: false, JSON: false, XML: true, Img: false, Other: false },
 					})}
 			>
 				<span class="label-text">XML</span>
+				{#if $settings.filters.XML}<Fa icon={faCheckCircle} class="text-amber-400" />{/if}
 			</button>
 		</li>
 		<li>
 			<button
-				class="form-control"
+				class="form-control justify-between"
 				onclick={() =>
 					setSettings({
 						filters: { GQL: false, JSON: false, XML: false, Img: true, Other: false },
 					})}
 			>
 				<span class="label-text">Img</span>
+				{#if $settings.filters.Img}<Fa icon={faCheckCircle} class="text-amber-400" />{/if}
 			</button>
 		</li>
 		<li>
 			<button
-				class="form-control"
+				class="form-control justify-between"
 				onclick={() =>
 					setSettings({
 						filters: { GQL: false, JSON: false, XML: false, Img: false, Other: true },
 					})}
 			>
 				<span class="label-text">Other</span>
+				{#if $settings.filters.Other}<Fa icon={faCheckCircle} class="text-amber-400" />{/if}
 			</button>
 		</li>
 	</ul>
